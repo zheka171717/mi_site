42d74946e4f4e2e7609a2acd0712a17e1b3c8588
zheka171717 <zhena.kondrashov@mail.ru>
2025-06-08 19:27:55 +0300

    Update site.js

--- site.js
+++ site.js
@@ -1,1 +1,12 @@
+function toggleTheme() {
+  document.body.classList.toggle('dark-theme');
+  const theme = document.body.classList.contains('dark-theme') ? 'dark' : 'light';
+  localStorage.setItem('theme', theme);
+}
 
+window.addEventListener('DOMContentLoaded', () => {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === 'dark') {
+    document.body.classList.add('dark-theme');
+  }
+});
